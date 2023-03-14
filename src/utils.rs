486787072ca0f87a9871de2eee3fd0739bb990f8
lokuhra <leandro.88.bernardi@gmail.com>
2023-03-14 20:48:41 -0300

    return axum 5

--- src/utils.rs
+++ src/utils.rs
@@ -2,9 +2,8 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use axum::{
     async_trait,
-    extract::FromRequestParts,
+    extract::{FromRequest, RequestParts},
     headers::{authorization::Bearer, Authorization},
-    http::request::Parts,
     TypedHeader,
 };
 use jsonwebtoken::{decode, Validation};
@@ -21,41 +20,19 @@ pub fn get_timestamp_8_hours_from_now() -> u64 {
 
 // verify token and extract data from it (a kind of middleware), whenever you try to extract claims in the handle it will first run this code
 #[async_trait]
-impl<S> FromRequestParts<S> for Claims
+impl<B> FromRequest<B> for Claims
 where
-    S: Send + Sync,
+    B: Send,
 {
     type Rejection = AppError;
 
-    async fn from_request_parts(req: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let TypedHeader(Authorization(bearer_token)) =
-            TypedHeader::<Authorization<Bearer>>::from_request_parts(req, state)
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) =
+            TypedHeader::<Authorization<Bearer>>::from_request(req)
                 .await
-                .map_err(|_| AppError::MissingCredential)?;
-
-        let header = decode_header(bearer_token.token())?;
-
-        let kid = match header.kid {
-            Some(k) => k,
-            None => return Err(AppError::InvalidToken),
-        };
-
-        let jwks = get_jwks().await?;
-
-        let decoded = match jwks.find(&kid) {
-            Some(j) => match j.algorithm {
-                AlgorithmParameters::RSA(ref rsa) => {
-                    let decoding_key = DecodingKey::from_rsa_components(&rsa.n, &rsa.e).unwrap();
-                    let validation = Validation::new(j.common.algorithm.unwrap());
-
-                    decode::<Claims>(bearer_token.token(), &decoding_key, &validation)
-                        .map_err(AppError::MissingCredential)
-                }
-                _ => Err(AppError::InvalidToken),
-            },
-            None => Err(AppError::InvalidToken),
-        }?;
-
-        Ok(decoded.claims)
+                .map_err(|_| AppError::InvalidToken)?;
+        let data = decode::<Claims>(bearer.token(), &KEYS.decoding, &Validation::default())
+            .map_err(|_| AppError::InvalidToken)?;
+        Ok(data.claims)
     }
 }
